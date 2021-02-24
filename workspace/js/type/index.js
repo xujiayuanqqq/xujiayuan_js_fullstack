094018b2005812fc32a83a879fecff85d45eeb39
xujiayuan <xujiayuan@inrice.com>
2021-02-24 11:40:41 +0800

    fix: 🐛 delete unuseless code

---
+++ workspace/js/type/index.js
@@ -0,0 +1,9 @@
+console.log(typeof 1) // number
+console.log(typeof '1') // string
+console.log(typeof undefined) // undefined
+console.log(typeof true) // boolean
+console.log(typeof Symbol()) // symbol
+console.log(typeof b) // 没有声明，但输出为undefined
+console.log(typeof null) // object
+
+console.log(typeof new Array()) // object
---
+++ workspace/js/type/readme.md
@@ -0,0 +1,9 @@
+js 内置类型 基本类型和对象
+基本类型：String Number Boolean Null Undefined Symbol
+对象：Object 引用类型，会遇到深拷贝和浅拷贝
+js的数字类型是浮点类型，没有整型。NaN也属于Number类型，NaN不等于自身
+
+typeOf
+typeof对于基本类型，除了null都可以显示正确的类型
+
+instanceOf
